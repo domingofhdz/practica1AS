0c56e6dc3e70579ef728619d20f2b0651d8615c7
Domingo Fraga Hernandez <desarrolladorrapido@gmail.com>
2025-09-03 17:18:22 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -58,6 +58,20 @@ app.run(["$rootScope", "$location", "$timeout", function($rootScope, $location, 
     $rootScope.$on("$routeChangeSuccess", function (event, current, previous) {
         $("html").css("overflow-x", "hidden")
 
+        document.addEventListener("DOMContentLoaded", function () {
+            // Enable pusher logging - don't include this in production
+            Pusher.logToConsole = true;
+        
+            var pusher = new Pusher("e57a8ad0a9dc2e83d9a2", {
+              cluster: "us2"
+            })
+        
+            var channel = pusher.subscribe("canalProductos")
+            channel.bind("eventoProductos", function(data) {
+                alert(JSON.stringify(data))
+            })
+        })
+        
         const path = current.$$route.originalPath
 
         if (path.indexOf("splash") == -1) {
@@ -142,6 +156,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     activeMenuOption(location.hash)
 })
+
 
 
 
