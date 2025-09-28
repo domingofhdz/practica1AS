274639af5c624dca8cc2645d6492f9384608cef6
Domingo Fraga Hernandez <desarrolladorrapido@gmail.com>
2025-09-27 21:27:54 -0500

    Update app js spinner tbody.

--- static/js/app.js
+++ static/js/app.js
@@ -327,6 +327,13 @@ app.controller("appCtrl", function ($scope, $http) {
 })
 app.controller("productosCtrl", function ($scope, $http, $rootScope) {
     function buscarProductos() {
+        $("#tbodyProductos").html(`<tr>
+            <th colspan="5" class="text-center">
+                <div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+                    <span class="visually-hidden">Cargando...</span>
+                </div>
+            </th>
+        </tr>`)
         $.get("productos/buscar", {
             busqueda: ""
         }, function (productos) {
